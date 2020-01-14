834e468ac9d30b33a435d3728618a6ff1aa0adbe
Patricio R <patricio.raschetti@gmail.com>
2020-01-14 20:29:16 -0300

    Hook and setup basic Socket.io and test WS connection.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const http = require('http');
 const path = require('path');
+const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const settings = {
     port: process.env.PORT,
@@ -9,7 +13,12 @@ const settings = {
 };
 
 app.use(express.static(settings.publicDir));
+app.use(express.json());
 
-app.listen(settings.port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection');
+});
+
+server.listen(settings.port, () => {
     console.log(`Server is up and running on port ${settings.port}!`);
 });
