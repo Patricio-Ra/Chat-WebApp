895058b1272bb80273afa830c70a400371ac5cd4
Patricio R <patricio.raschetti@gmail.com>
2020-01-14 03:06:52 -0300

    Add env-cmd and nodemon modules for 'dev' run script

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Settings.
+const settings = {
+    port: process.env.PORT,
+    publicDir: path.join(__dirname, '../public'),
+};
+
+app.use(express.static(settings.publicDir));
+
+app.listen(settings.port, () => {
+    console.log(`Server is up and running on port ${settings.port}`);
+});
