b8f0bcaf044c7c0adc5e67c2c7769f67099e49fc
Patricio R <patricio.raschetti@gmail.com>
2020-04-28 08:43:42 -0300

    eslint refactor

--- src/index.js
+++ src/index.js
@@ -13,22 +13,30 @@ const io = socketio(server);
 // Express/HTTP Server
 const settings = {
     port: process.env.PORT,
-    publicDir: path.join(__dirname, '../public'),
+    publicDir: path.join(__dirname, '../public')
 };
 
 app.use(express.static(settings.publicDir));
 app.use(express.json());
 
 // Socket.io/WS Server
-io.on('connection', (socket) => {
+io.on('connection', socket => {
     console.log('New WebSocket connection.');
 
     socket.on('join', ({ username, room }, ackCallback) => {
         const { error, user } = addUser({ id: socket.id, username, room });
         if (error) return ackCallback(error);
         socket.join(user.room);
-        socket.emit('message', generateMessage(`Welcome, ${user.showUsername}!`));
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.showUsername} has joined the room!`));
+        socket.emit(
+            'message',
+            generateMessage(`Welcome, ${user.showUsername}!`)
+        );
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                generateMessage(`${user.showUsername} has joined the room!`)
+            );
         io.to(user.room).emit('roomData', {
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -42,29 +50,37 @@ io.on('connection', (socket) => {
         const filter = new Filter();
         if (filter.isProfane(message)) {
             return ackCallback('Profanity is not allowed.');
-        };
-        io.to(user.room).emit('message', generateMessage(message, user.showUsername));
+        }
+        io.to(user.room).emit(
+            'message',
+            generateMessage(message, user.showUsername)
+        );
         ackCallback();
     });
 
     socket.on('sendLocation', ({ latitude, longitude }, ackCallback) => {
         const user = getUser(socket.id);
-        io.to(user.room).emit('locationMessage', generateLocationMessage(user.showUsername, latitude, longitude));
+        io.to(user.room).emit(
+            'locationMessage',
+            generateLocationMessage(user.showUsername, latitude, longitude)
+        );
         ackCallback();
     });
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.showUsername} has left the chat!`));
+            io.to(user.room).emit(
+                'message',
+                generateMessage(`${user.showUsername} has left the chat!`)
+            );
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
             });
-        };
+        }
     });
 });
-
 
 server.listen(settings.port, () => {
     console.log(`Server is up and running on port ${settings.port}!`);
